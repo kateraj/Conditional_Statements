d40f3e4fe9d04e54d1e5755851efe9d824c6c04e
Stylebykat <Stylebykat@LAPTOP-OJ9JTFP6>
2020-09-17 22:49:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Author: Katera Jefferson
+Date: 9/17/2020
+*/
+
+using System;
+using System.Linq.Expressions;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What grade do you expect to get in ISM 4300?");
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+            if (value_of_input >= 90) Console.WriteLine("Grade A");
+            else if ((value_of_input >= 70) && (value_of_input < 90)) Console.WriteLine("Grade B");
+            else if ((value_of_input >= 50) && (value_of_input < 70)) Console.WriteLine("Grade C");
+            else if (value_of_input < 50) Console.WriteLine("Grade F");
+            else Console.WriteLine("Invalid input");
+            }
+            catch
+            {
+            }
+        }
+    }
+}
